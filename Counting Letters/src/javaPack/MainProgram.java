66aed1c41dbf9d8e30e96c1a2ca27fce5e24a0b3
ashleyhrv2 <168749264+ashleyhrv2@users.noreply.github.com>
2025-12-02 07:53:01 -0500

    Add files via upload

---
+++ Counting Letters/src/javaPack/MainProgram.java
@@ -0,0 +1,9 @@
+package javaPack;
+
+public class MainProgram 
+{
+	public static void main(String [] args)
+	{
+		UseChar allinfo = new UseChar();
+	}
+}
---
+++ Counting Letters/src/javaPack/UseChar.java
@@ -0,0 +1,37 @@
+package javaPack;
+import java.util.Scanner;
+
+public class UseChar 
+{
+	Scanner allinput = new Scanner(System.in);
+	public UseChar()
+	{
+		System.out.print("Enter a word: ");
+		String vowelword = allinput.nextLine();
+		int numvowels = CountVowels(vowelword);
+		System.out.println("The number of vowels are: " + numvowels );
+	}
+	
+	private static int CountVowels(String wordvowel)
+	{
+		int numofvowels=0, wordlen=0, notvowel=0;
+		wordlen = wordvowel.length();
+		for (int i=0; i<wordlen; i++)
+		{
+			char whichletter = wordvowel.charAt(i);
+			System.out.print(whichletter + " ");
+			switch(whichletter)
+			{
+			case 'a': case 'e': case 'i': case 'o': case 'u':
+				numofvowels = numofvowels + 1;
+				break;
+			default:
+				notvowel = notvowel + 1;
+				break 
+				
+					
+			}
+		}
+	}
+
+}
